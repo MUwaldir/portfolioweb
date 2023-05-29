4953176b3d2c7ca7be6363cd18d1c94a541dbb4f
Waldir merjildo <muwaldir@gmail.com>
2023-05-29 10:30:27 -0500

    manejo del redirect y poner fondo antes que crga cover

--- src/components/cover/Cover.jsx
+++ src/components/cover/Cover.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Cover.css";
 import coverVideo from "../../media/coverVideo.mp4";
 
 const Cover = () => {
+  const [videoLoaded, setVideoLoaded] = useState(false);
+
+  const handleVideoLoaded = () => {
+    setVideoLoaded(true);
+  };
+
   return (
-    <div className="cover-container">
-      <video className="video" src={coverVideo} autoPlay loop muted />
+    <div className={!videoLoaded ? "cover_negro": "cover-container" } >
+      
+      <video className="video" onLoadedData={handleVideoLoaded} src={coverVideo} autoPlay loop muted />
       <h1>Waldir Merjildo Ubaldo</h1>
       <p>System Enginiering | Developer web | Front-end | Back-end | Henry Student </p>
     </div>
